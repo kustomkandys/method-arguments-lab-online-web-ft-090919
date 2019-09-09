6d336a8e6c5f65b274e2608d71289ba3cb1649ca
Keri Deemer <kerid18@att.net>
2019-09-09 14:34:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction(name, language)
-  "Hi my name is #{ name } and I am learning #{language}."
-end
-
 name = "Keri"
 language = "Ruby"
 
-introduction
+def introduction(name, language)
+  puts "Hi my name is #{ name } and I am learning #{language}."
+end
+
+
